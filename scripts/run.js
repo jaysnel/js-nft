9cabe74545ba9a4585aaef49f760bf8e7b6cae3d
jaysnel <jaylan210@gmail.com>
2022-10-12 15:05:09 -0500

    simple contract deploying. Default installed hardhat code removed

---
+++ scripts/run.js
@@ -0,0 +1,19 @@
+const deployContract = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('JSNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+  };
+
+
+const main = async () => {
+    try {
+      await deployContract();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  main();
