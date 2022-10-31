1d8b253c8caf6575bec978522e0393c8f60671ac
jaysnel <jaylan210@gmail.com>
2022-10-31 11:50:57 -0500

    For now can deploy one NFT at a time

--- contracts/JSNFT.sol
+++ contracts/JSNFT.sol
@@ -11,14 +11,6 @@ import "hardhat/console.sol";
 contract JSNFT is ERC721URIStorage {
     uint256 _user_nft_number;
     string _nftJsonData;
-    
-    event EmitUserCreatedNFT(string _nftData);
-
-    struct UserNFTData {
-        string _nftData;
-    }
-
-    UserNFTData[] userNft;
 
     // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
     using Counters for Counters.Counter;
@@ -33,32 +25,20 @@ contract JSNFT is ERC721URIStorage {
     function makeJSNFT() public {
         // Get the current tokenId, this starts at 0.
         uint256 newItemId = _tokenIds.current();
-
         // Actually mint the NFT to the sender using msg.sender.
         _safeMint(msg.sender, newItemId);
-
         // Increment the counter for when the next NFT is minted.
         _tokenIds.increment();
     }
 
     function setNFTData(uint256 _itemID, string memory _nftData) public {
         _user_nft_number = _itemID;
-        console.log(_nftData);
         _nftJsonData = Base64.encode(bytes(_nftData));
-        userNft.push(UserNFTData(_nftData));
-        emit EmitUserCreatedNFT(_nftData);
-    }
-
-    function getNFTData() public view returns (UserNFTData[] memory){
-        return userNft;
     }
 
     // Set the NFT's metadata
     function tokenURI(uint256 _tokenId) public view override returns (string memory) {
         require(_exists(_tokenId));
-        // getNFTData();
-        // console.log(string.concat("data:application/json;base64", _nftJsonData));
-        // console.log("An NFT SVG w/ ID %s has been minted to %s", _tokenId, msg.sender);
         return string(
             abi.encodePacked(
                 "data:application/json;base64",
--- scripts/run.js
+++ scripts/run.js
@@ -4,17 +4,19 @@ const deployContract = async () => {
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
     const aiImages = [
-      '{ "name": "JS-NFT-AI-ART", "description": "AI art created by JS-NFT", "image": "https://www.dropbox.com/s/toq98op6t8g9wts/AI-POST-WORD-1.jpeg?raw=1" }', 
-      '{ "name": "JS-NFT-AI-ART-2", "description": "AI art created by JS-NFT", "image": "https://www.dropbox.com/s/o5h5f9deg7nm5cj/AI-POST-WORLD-2.jpeg?raw=1" }'
+      '{ "name": "JS-NFT-AI-ART-2", "description": "AI art created by JS-NFT", "image": "https://www.dropbox.com/s/o5h5f9deg7nm5cj/AI-POST-WORLD-2.jpeg?raw=1" }',
+      '{ "name": "JS-NFT-AI-ART", "description": "AI art created by JS-NFT", "image": "https://www.dropbox.com/s/toq98op6t8g9wts/AI-POST-WORD-1.jpeg?raw=1" }'
     ]
 
     let itemID = 0
     while(itemID <= aiImages.length - 1) {
       let txn = await nftContract.makeJSNFT()
       await txn.wait()
-      // await await nftContract.setBase64NFTData('{ "name": "JS-NFT-AI-ART", "description": "AI art created by JS-NFT", "image": "https://www.dropbox.com/s/toq98op6t8g9wts/AI-POST-WORD-1.jpeg?raw=1" }')
       await nftContract.setNFTData(itemID, aiImages[itemID])
       await nftContract.tokenURI(itemID)
+      // let data = await nftContract.getNFTData();
+      // console.log(data[0][0])
+      // console.log('Minted NFT #%s with data: %s', itemID, aiImages[itemID])
       itemID++;
     }
   };
